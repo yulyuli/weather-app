fb7c5e09e6e44bd0d5b69e86d031cab272dac9f4
Yulia <yuliayurchenkodp@gmail.com>
2023-06-13 15:21:54 +0300

    Created JS from HTML for forecast(concatenation)

--- weather-app.js
+++ weather-app.js
@@ -39,6 +39,35 @@ function displayWeatherCondition(response) {
     "Wind: " + Math.round(response.data.wind.speed) + "km/h";
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
+
 function searchForm(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
